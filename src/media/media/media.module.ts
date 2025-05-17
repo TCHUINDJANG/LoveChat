74a4719b73f7e3e07d5f82c9c075d75b1f01c8df
unknown <paulnicolas519@gmail.com>
2025-05-17 14:48:48 +0200

    feature/fichiers

---
+++ src/media/media/media.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MediaService } from './media.service';
+import { MediaController } from './media.controller';
+import { UploadService } from 'src/upload/upload/upload.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Media } from '../entity/media';
+import { User } from 'src/user/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([Media , User]), // Ceci fournit le MediaRepository
+  ],
+  providers: [MediaService , UploadService ],
+  controllers: [MediaController],
+  exports: [MediaService]
+})
+export class MediaModule {}
